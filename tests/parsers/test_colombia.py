8c95c34ea831a780b584cc28890a1f912865c567
pablo.torresgutierrez <pablo.torresgutierrez@cencosud.cl>
2025-07-07 18:40:39 -0400

    UPDATE: Add Selenium Based Retieval Code Test

--- tests/parsers/test_colombia.py
+++ tests/parsers/test_colombia.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import pytest
 
+from cpilatam.parsers.base import CPI_SCHEMA
 from cpilatam.parsers.colombia import ColombiaCPIParser
-from cpilatam.schemas import CPI_SCHEMA
+from cpilatam.retrievers.colombia import ColombiaCPIRetriever
 
 
 class TestColombiaParser:
@@ -10,13 +11,18 @@ class TestColombiaParser:
     def setUp(self, monkeypatch):
         # crear instancia
         self.parser = ColombiaCPIParser()
+        self.retriever = ColombiaCPIRetriever()
 
         # mock download using monkeypatch
-        monkeypatch.setattr(self.parser, "download", self.mock_download)
+        monkeypatch.setattr(self.parser, "download", self.mock_download_parser)
+        monkeypatch.setattr(self.retriever, "download", self.mock_download_retrieval)
 
-    def mock_download(self):
+    def mock_download_parser(self):
         # Read the data and assign it to self.data
-        self.parser.data = pd.read_excel("tests/data/colombia.xlsx")
+        self.parser.data = pd.read_excel("data/raw/colombia.xlsx")
+
+    def mock_download_retrieval(self):
+        return "cpilatam/retrievers/retrieved_files/colombia_cpi.xlsx", False
 
     def test_parse(self, setUp):
         # read data
@@ -27,3 +33,10 @@ class TestColombiaParser:
 
         # assert that the schema is correct
         CPI_SCHEMA.validate(self.parser.data)
+
+    def test_retrieval(self, setUp):
+        # read data
+        path, error = self.retriever.download()
+
+        # parse and validate the data
+        CPI_SCHEMA.validate(self.retriever.parse(path))
